057747a45ff9435597710367146330a2979dea0e
TijsT <ttaalt@gmail.com>
2021-06-19 13:19:03 +0200

    logout button

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,6 +4,13 @@
     <title>CRUD Laravel 8</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
+<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+    Logout
+</a>
+
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    {{ csrf_field() }}
+</form>
 <body>
 
 <div class="container">
